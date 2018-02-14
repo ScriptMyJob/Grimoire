99dc12e1f0521cc1006b62fbbc9b1ce8cc7bbd95
Robert J <robert@scriptmyjob.com>
2018-02-14 06:57:28 -0600

    Added nginx configs, removed Ansible terraform inventory in favor of Dynamic Inventory, Working towards cleaning up this project.

--- Terraform/ansible_vars.tf
+++ Terraform/ansible_vars.tf
@@ -2,9 +2,19 @@ resource "local_file" "ansible_mysql_vars" {
     filename    = "${path.module}/../Ansible/roles/mysql/vars/main.yml"
     content     = <<VARS
 {
-    rds_endpoint: "${aws_rds_cluster.scriptmyjob_shared_db.endpoint}",
+    rds_endpoint: "${lookup(var.rds,"endpoint")}",
     rds_user: "${lookup(var.rds, "db_user")}",
-    rds_password: "${lookup(var.rds, "db_password")}"
+    rds_password: "${lookup(var.rds, "db_passwd")}"
 }
 VARS
 }
+
+resource "local_file" "ansible_nginx_configs_vars" {
+    filename    = "${path.module}/../Ansible/roles/nginx_configs/vars/main.yml"
+    content     = <<VARS
+{
+    site_name: "${lookup(var.global,"site_name")}"
+}
+VARS
+}
+
